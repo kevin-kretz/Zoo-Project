8930cd93a68b88abfeb20fa474f8112af714d08e
Kevin Kretz <kkretz17@gmail.com>
2019-02-24 13:18:03 -0700

    Update and rename MD5Digest.java to documentation/MD5Digest.java
    Moved file to documentation folder for organization and edited document to show it was provided by SNHU.

--- documentation/MD5Digest.java
+++ documentation/MD5Digest.java
@@ -1,19 +1,19 @@
+//This code was provided by SNHU to be used for this project.
+
 import java.security.MessageDigest;
 
 public class MD5Digest {
 
 	public static void main(String[] args) throws Exception {
       
-      //Copy and paste this section of code
 		String original = "letmein";  //Replace "password" with the actual password inputted by the user
 		MessageDigest md = MessageDigest.getInstance("MD5");
 		md.update(original.getBytes());
 		byte[] digest = md.digest();
-      StringBuffer sb = new StringBuffer();
+      		StringBuffer sb = new StringBuffer();
 		for (byte b : digest) {
 			sb.append(String.format("%02x", b & 0xff));
 		}
-      //End copy/paste
 
 		System.out.println("original:" + original);
 		System.out.println("digested:" + sb.toString()); //sb.toString() is what you'll need to compare password strings
